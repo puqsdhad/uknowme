ee40ec779da202c78663e69e0010b526f28d67db
sheh <falzzzneverlus@gmail.com>
2026-07-03 08:59:53 +0700

    Update

---
+++ pyrogram/methods/chats/create_forum_topic.py
@@ -0,0 +1,41 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+import pyrogram
+from pyrogram import raw
+from pyrogram import types
+from typing import Union
+
+class CreateForumTopic:
+    async def create_forum_topic(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        title: str,
+        icon_color: int = None,
+        icon_emoji_id: int = None
+    ) -> "types.ForumTopicCreated":
+        r = await self.invoke(
+            raw.functions.messages.CreateForumTopic(
+                peer=await self.resolve_peer(chat_id),
+                title=title,
+                random_id=self.rnd_id(),
+                icon_color=icon_color,
+                icon_emoji_id=icon_emoji_id
+            )
+        )
+        return types.ForumTopicCreated._parse(r.updates[1].message)
