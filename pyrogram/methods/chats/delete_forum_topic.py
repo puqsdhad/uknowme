7ec3019805caa5bbb0eccbddc0e0969057b9ddbf
sheh <falzzzneverlus@gmail.com>
2026-07-03 08:59:08 +0700

    Update

---
+++ pyrogram/methods/chats/delete_forum_topic.py
@@ -0,0 +1,36 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+import pyrogram
+from pyrogram import raw
+from pyrogram import types
+from typing import Union
+
+class DeleteForumTopic:
+    async def delete_forum_topic(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        topic_id: int
+    ) -> bool:
+        await self.invoke(
+            raw.functions.messages.DeleteTopicHistory(
+                peer=await self.resolve_peer(chat_id),
+                top_msg_id=topic_id
+            )
+        )
+        return True
